42b221d6de0a8ce47b6e3b77c7e604c6c450c599
Fabian Rozas <fabian.rozas@alumnos.uv.cl>
2021-06-05 12:21:39 -0400

    operadores

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//Declaración de constantes
@@ -28,4 +30,56 @@ func main() {
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	//Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	//Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	//Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	//División
+	result = y / x
+	fmt.Println("División:", result)
+
+	//Modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	//Incrementar
+	x++
+	fmt.Println("Incremental:", x)
+
+	//Decremento
+	x--
+	fmt.Println("Decremental:", x)
+
+	//Retos Área
+	//Rectángulo, trapecio y de un Círculo
+
+	//Área rectángulo
+	const baseRectangulo = 18
+	const alturaRectangulo = 12
+	areaRectangulo := baseRectangulo * alturaRectangulo
+	fmt.Println("Área rectángulo:", areaRectangulo)
+
+	//Área trapecio
+	const baseMenorTrapecio = 3
+	const baseMayorTrapecio = 5
+	const alturaTrapecio = 4
+	areaTrapecio := ((baseMayorTrapecio + baseMenorTrapecio) / 2) * alturaTrapecio
+	fmt.Println("Área trapecio:", areaTrapecio)
+
+	//Área círculo
+	const radioCirculo = 5
+	areaCirculo := pi * (radioCirculo * radioCirculo)
+	fmt.Println("Área circulo:", areaCirculo)
 }
